62178d3ea1e9069f7230a84977cb3a4e450299ed
Rizal Anggoro <gnoogler4@gmail.com>
2025-09-22 06:21:48 +0700

    update message format in API testing script to include current date

--- sister-tugas-4/api.js
+++ sister-tugas-4/api.js
@@ -8,11 +8,13 @@ export const options = {
 
 export default function () {
   const url = "https://api-sister-tugas-4.rizalanggoro.space/global-messages";
+  const now = new Date();
+  const randomMessage = `Hello from k6! Current date: ${now.toISOString()}`;
   let res = http.post(
     url,
     JSON.stringify({
       name: "k6",
-      message: "Hello from k6 load testing tool!",
+      message: randomMessage,
     })
   );
 
