9de54e8a76a36500bf222c4a77cfe499cb76b713
Rizal Anggoro <gnoogler4@gmail.com>
2025-09-24 16:08:39 +0700

    add K6 workflow scripts for gRPC and REST APIs

---
+++ sister-tugas-4/dummy-api-grpc.js
@@ -0,0 +1,25 @@
+import { check } from "k6";
+import http from "k6/http";
+
+export const options = {
+  vus: 1000,
+  duration: "60s",
+};
+
+export default function () {
+  const url =
+    "https://api-sister-tugas-4.rizalanggoro.space/dummy-global-messages/grpc";
+  const now = new Date();
+  const randomMessage = `Hello from k6 using gRPC! Current date: ${now.toISOString()}`;
+  let res = http.post(
+    url,
+    JSON.stringify({
+      name: "k6",
+      message: randomMessage,
+    })
+  );
+
+  check(res, {
+    "status 200": (r) => r.status === 200,
+  });
+}
